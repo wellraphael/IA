6ddbe1f74aa6e66baa9fa702db928e6f3b1dc5e5
wellraphael <166656921+wellraphael@users.noreply.github.com>
2025-09-15 11:28:31 -0300

    .

--- script.js
+++ script.js
@@ -12,4 +12,32 @@ alternativas: [
     "Alternativa 2"
 ]       
     }, 
-]
+     {
+enunciado: "Pergunta 2",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+]       
+    },
+     {
+enunciado: "Pergunta 3",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+]       
+    },
+     {
+enunciado: "Pergunta 4",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+]       
+    },
+     {
+enunciado: "Pergunta 5",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+]       
+    }
+];
