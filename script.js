08dac612d304dbffd1209d7c785c78fa3b362dd8
wellraphael <166656921+wellraphael@users.noreply.github.com>
2025-09-15 11:23:38 -0300

    .

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = 
+const perguntas = [
+    {
+enunciado: "Pergunta 1",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+]       
+    }, 
+]
